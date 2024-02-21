828ef721077855c8f78d824abd9213923306df73
abhi-develope <prajapatiabhishek320@gmail.com>
2024-02-21 23:25:51 +0530

    create contact section

--- contactPage/src/App.jsx
+++ contactPage/src/App.jsx
@@ -1,11 +1,13 @@
 
 import './App.css'
+import ContactHeader from './components/ContactHeader/ContactHeader'
 import Navbar from './components/Navbar'
 
 function App() {
   return (
     <>
      <Navbar/>
+     <ContactHeader/>
     </>
   )
 }
---
+++ contactPage/src/components/ContactHeader/ContactHeader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ContactHeader = () => {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet molestias provident placeat quisquam repellat accusantium beatae. Maxime fugit aspernatur vero, nihil, laudantium saepe aut blanditiis at quos est a quae!</p>
+    </div>
+  )
+}
+
+export default ContactHeader
