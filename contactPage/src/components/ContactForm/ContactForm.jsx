4cfcf9560c3bcd93a96a34a70857e4400d43836e
abhi-develope <prajapatiabhishek320@gmail.com>
2024-02-23 18:13:23 +0530

    creat form and add img

--- contactPage/src/components/ContactForm/ContactForm.jsx
+++ contactPage/src/components/ContactForm/ContactForm.jsx
@@ -14,13 +14,23 @@ const ContactForm = () => {
       </div>
       <Button isOutline = {true} text='VIA EMAIL' icon={<MdOutlineMail fontSize='1.8rem' />}/>
 
-      <form action="">
+      <form>
         <div className={styles.form_control}>
         <label htmlFor="name">Name</label>
         <input type="text" name='name' />
         </div>
+        <div className={styles.form_control}>
+        <label htmlFor="email">Email</label>
+        <input type="email" name='email' />
+        </div>
+        <div className={styles.form_control}>
+        <label htmlFor="text">Text</label>
+        <textarea  name='text' rows="8" />
+        </div>
+        <div style={{display: "flex",justifyContent: "end"}}><Button text='SUBMIT BUTTON' /></div>
       </form>
      </div>
+     <div className={styles.contact_image}><img src="images/img2.jpg" alt="" /></div>
     </section>
   )
 }
