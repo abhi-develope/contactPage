13872dbf934f6158c9d40bcd58f403704c84050e
abhi-develope <prajapatiabhishek320@gmail.com>
2024-02-29 04:59:42 +0530

    final changes

--- contactPage/src/components/ContactForm/ContactForm.jsx
+++ contactPage/src/components/ContactForm/ContactForm.jsx
@@ -27,7 +27,10 @@ const ContactForm = () => {
         <label htmlFor="text">Text</label>
         <textarea  name='text' rows="8" />
         </div>
-        <div style={{display: "flex",justifyContent: "end"}}><Button text='SUBMIT BUTTON' /></div>
+        <div 
+        style={{display: "flex",justifyContent: "end"}}>
+        <Button text='SUBMIT BUTTON' />
+        </div>
       </form>
      </div>
      <div className={styles.contact_image}><img src="images/contact.png" alt="" /></div>
